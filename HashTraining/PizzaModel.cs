b3ec3de9e73352cf095a19b3fcc312d8339edfb9
Noixelfer <pauldarius98@gmail.com>
2020-02-09 17:06:54 +0200

    Create Pizza Model

---
+++ HashTraining/PizzaModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace HashTraining
+{
+	class PizzaModel
+	{
+		public int NumberOfSlices { get; set; }
+		public int NumberOfTypes { get; set; }
+		public IEnumerable<int> Slices { get; set; }
+	}
+}
