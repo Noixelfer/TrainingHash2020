b65dec25d947c5d6143f0a2144e0c56f50eec4bd
Noixelfer <pauldarius98@gmail.com>
2020-02-20 20:29:14 +0200

    wip: Add data writing

--- HashTraining/BookScanningOutput.cs
+++ HashTraining/BookScanningOutput.cs
@@ -6,5 +6,12 @@ namespace HashTraining
 	{
 		public int NumberOfScannedLibraries { get; set; }
 		public List<(int, List<int>)> ScannedLibraries { get; set; }
+		public int Score
+		{
+			get
+			{
+				return 0;
+			}
+		}
 	}
 }
