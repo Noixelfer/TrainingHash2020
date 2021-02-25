6d6f3c309b3210630a72e4983329c952caf29bb5
Paul Ciurea <ciureapaul@gmail.com>
2021-02-25 19:59:54 +0200

    Add input models

--- HashTraining/Models/InputModel.cs
+++ HashTraining/Models/InputModel.cs
@@ -1,6 +1,29 @@
-﻿namespace HashTraining.Models
+﻿using System.Collections.Generic;
+
+namespace HashTraining.Models
 {
     public class InputModel
     {
+        public int SimulationDuration;
+        public int IntersectionCount;
+        public int NumberOfStreets;
+        public int NumberOfCars;
+        public int CarBonusPoints;
+
+        public List<Street> Streets;
+    }
+    
+    public class Street
+    {
+        public int IntersectionStart;
+        public int IntersectionEnd;
+        public string Name;
+        public int Length;
+    }
+
+    public class CarPath
+    {
+        public int StreetCount;
+        public List<string> StreetNames;
     }
 }
